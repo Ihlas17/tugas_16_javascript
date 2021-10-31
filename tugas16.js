f09f69f69b0064d3fb8ef75b556781f247e4ef8f
ihlas_sul <ihlassmada@gmail.com>
2021-11-01 07:40:06 +0800

    tugas 16 javascript
    final

--- tugas16.js
+++ tugas16.js
@@ -1,4 +1,4 @@
-let angkaMax = Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10)
+let angkaMax = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5)
 let angkaMin = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10)
 console.log('nilai maximal : '+angkaMax)
 console.log('nilai minimal : '+angkaMin)
